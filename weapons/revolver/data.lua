b8886f825a86ce0a2373272d30aedbb484a17f5c
althmy <141419932+althmy@users.noreply.github.com>
2023-08-08 01:20:51 +0300

    Update data.lua

--- weapons/revolver/data.lua
+++ weapons/revolver/data.lua
@@ -1,6 +1,8 @@
 local data = {
 	["ANIM_idle"] = "rbxassetid://14335987777",
-	["ANIM_shoot"] = "rbxassetid://14336549190"
+	["ANIM_shoot"] = "rbxassetid://14336549190",
+	
+	["BULLETS_count"] = 6
 }
 
 
