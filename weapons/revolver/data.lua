ba7854986e1ae4204659a9dcb3a0b2d67009a847
kottz-git <141419932+kottz-git@users.noreply.github.com>
2023-08-13 21:25:49 +0300

    Update data.lua

--- weapons/revolver/data.lua
+++ weapons/revolver/data.lua
@@ -1,6 +1,12 @@
 local data = {
 	["ANIM_idle"] = "rbxassetid://14335987777",
 	["ANIM_shoot"] = "rbxassetid://14336549190",
+	["REVOLVER_gunshot_sound"] = "rbxassetid://800001986",
+	["REVOLVER_equip_sound"] = "rbxassetid://3603367478",
+	["REVOLVER_reload_sound"] = "rbxassetid://14413005092",
+	
+	["REVOLVER_base_damage"] = 28,
+	["REVOLVER_knockback_focre"] = 55,
 	
 	["BULLETS_count"] = 6
 }
